a09008810dabfcd31eddd674054bb11014922c01
hy <hany@citydo.com.cn>
2020-02-15 12:32:56 +0800

    '框架'

---
+++ src/main/java/com/school/design/automobile_sales_mall/AutomobileSalesMallApplication.java
@@ -0,0 +1,13 @@
+package com.school.design.automobile_sales_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileSalesMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomobileSalesMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/school/design/automobile_sales_mall/AutomobileSalesMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.design.automobile_sales_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobileSalesMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
